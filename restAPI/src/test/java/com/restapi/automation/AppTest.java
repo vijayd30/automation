f6c6c92ba5779ff7aac39b1e0e9514f3ba0690d8
Vijay Deshmukh <vijaydeshmukh_99@yahoo.com>
2023-04-01 16:41:56 +0200

    Log4j configuration

--- restAPI/src/test/java/com/restapi/automation/AppTest.java
+++ restAPI/src/test/java/com/restapi/automation/AppTest.java
@@ -1,20 +1,28 @@
 package com.restapi.automation;
 
 import static org.junit.Assert.assertTrue;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
+public class AppTest
+    {
+    public static Logger logger = LogManager.getLogger(AppTest.class);
+        /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
+        assertTrue( true     );
+        logger.info("Test Message info");
+        logger.error("Test error message");
+        logger.debug("Test error message");
     }
 }
