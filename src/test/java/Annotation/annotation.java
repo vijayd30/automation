152a4ccf6d3f6e1dac16928dc8a1fb1c57c9fdb5
Vijay Deshmukh <vijaydeshmukh_99@yahoo.com>
2025-08-30 17:42:47 +0200

    Cucumber sample project

---
+++ src/test/java/Annotation/annotation.java
@@ -0,0 +1,29 @@
+package Annotation;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class annotation {
+    WebDriver driver = null;
+    @When("I enter username as {string}")
+    public void gotoYahoo (String name) {
+        driver = new ChromeDriver();
+        driver.get("https://login.yahoo.com");
+        WebElement we = driver.findElement(By.name("username"));
+        we.sendKeys(name);
+        System.out.println(name);
+    }
+
+    @When("I enter password as {string}")
+    public void enterPassword(String password) {
+        ;WebElement we = driver.findElement(By.name("signin"));
+        we.click();
+    }
+    @Then("Login should fail")
+    public void errorMessage() {
+
+    }
+}
